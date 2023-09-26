df8c6669b19be80f35bd52a73c987f02baca48e2
Kunal Sinha <kunal.sinha@verve.com>
2023-09-26 14:29:20 +0530

    MAX waterfall addition

---
+++ Pods/Headers/Private/AppLovinMediationVerveCustomNetworkAdapter/AppLovinMediationVerveCustomNetworkAdapter.h
@@ -0,0 +1,1 @@
+../../../AppLovinMediationVerveCustomNetworkAdapter/AppLovinAdapters/AppLovinMediationVerveCustomNetworkAdapter.h
---
+++ Pods/Headers/Private/HyBid/HyBidCustomClickUtil.h
@@ -0,0 +1,1 @@
+../../../HyBid/PubnativeLite/PubnativeLite/Core/HyBidCustomClickUtil.h
